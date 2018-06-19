884c6b1c8270a943a4d3f521bebdebe7aab9c7d6
Nathan Shepherd <ns057666@cerner.net>
2018-06-19 12:59:13 -0500

    messed stuff up real bad (twas one line)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,6 @@ class HomeController < ApplicationController
   def index
     @features = Feature.all
     @initiatives = Initiative.all
-    @quaters = Quater.all
     @roadmaps = Roadmap.all
     @teams = Team.all
   end
