b4ad8eb1a8b91c99f2c1ce70bb31ab93407a42cc
Nathan Shepherd <ns057666@cerner.net>
2018-06-27 14:55:06 -0500

    set up the structure of teams index (like the structure in team health on the excel)

--- app/helpers/teams_helper.rb
+++ app/helpers/teams_helper.rb
@@ -1,2 +1,27 @@
 module TeamsHelper
+
+  def create_lists(team)
+    @investments = {}
+    @features = {}
+    @initiatives = {}
+    @roadmaps = {}
+    Investment.where(team_id: team.id).each do |inv|
+      @investments.exclude?(inv) ? @investments[inv] = inv.investment : a=0
+      i = inv.investment
+      Feature.where(id: inv.feature_id).each do |feat|
+        @features.exclude?(feat) ? @features[feat] = i : @features[feat] += i
+        Initiative.where(id: feat.initiative_id).each do |init|
+          @initiatives.exclude?(init) ? @initiatives[init] = i : @initiatives[init] += i
+          Roadmap.where(id: init.roadmap_id).each do |map|
+            @roadmaps.exclude?(map) ? @roadmaps[map] = i : @roadmaps[map] += i
+          end
+        end
+      end
+    end
+    @investments = Hash[ @investments.sort_by {|key, val| key}]
+    @features = Hash[ @features.sort_by {|key, val| key}]
+    @initiatives = Hash[ @initiatives.sort_by {|key, val| key}]
+    @roadmaps = Hash[ @roadmaps.sort_by {|key, val| key}]
+  end
+
 end
